95a7c0108a6b4b0ca37407efc8eaa5b37b2cad51
Ankit Jain86 <Ankit.Jain86@ad.infosys.com>
2017-06-08 16:18:46 +0530

    added gulp

--- deploy.cmd
+++ deploy.cmd
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Run gulp transformations
 IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd .\node_modules\.bin\gulp imagemin
+  call :ExecuteCmd .\node_modules\.bin\gulp build
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
